8706191b00f2c624ff22ece29329a2b6fa150567
ElecTrOoD <32058883+ElecTrOoD@users.noreply.github.com>
2022-06-10 16:05:07 +0300

    changed filter to multiple for size and color

--- products/filters.py
+++ products/filters.py
@@ -5,8 +5,8 @@ from products.models import Product
 
 class ProductFilter(filters.FilterSet):
     category = filters.AllValuesMultipleFilter(field_name='categories__name', label='category')
-    color = filters.AllValuesFilter(field_name='stock__color__name', label='color')
-    size = filters.AllValuesFilter(field_name='stock__size__name', label='size')
+    color = filters.AllValuesMultipleFilter(field_name='stock__color__name', label='color')
+    size = filters.AllValuesMultipleFilter(field_name='stock__size__name', label='size')
     price = filters.RangeFilter(field_name='price')
 
     class Meta:
