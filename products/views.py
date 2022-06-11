22d2c62e61df32958eb5a4a1f19a8e477ccf21f7
ElecTrOoD <32058883+ElecTrOoD@users.noreply.github.com>
2022-06-12 01:16:19 +0300

    changed products viewset class to readonly

--- products/views.py
+++ products/views.py
@@ -1,11 +1,11 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from products.filters import ProductFilter
 from products.models import Product
 from products.serializers import ProductModelSerializer
 
 
-class ProductModelViewSet(ModelViewSet):
+class ProductModelViewSet(ReadOnlyModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductModelSerializer
     filterset_class = ProductFilter
